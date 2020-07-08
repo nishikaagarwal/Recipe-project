d24efb51c54a665090780a9c7789c1665eb4a991
nishikaagarwal <nishika991@gmail.com>
2020-07-08 12:59:15 +0530

    ingredients page

--- cooking/src/app/app-routing.module.ts
+++ cooking/src/app/app-routing.module.ts
@@ -11,6 +11,10 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'ingredients',
+    loadChildren: () => import('./ingredients/ingredients.module').then( m => m.IngredientsPageModule)
+  },
 ];
 
 @NgModule({
